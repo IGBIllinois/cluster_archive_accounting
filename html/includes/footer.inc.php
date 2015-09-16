1166e38d2ec4841057eb3923ad1b64a0401a2050
Joe Leigh <jleigh@jleigh.igb.illinois.edu>
2015-09-16 14:48:48 -0500

    Style cleanup - copyright line is static instead of fixed

--- html/includes/footer.inc.php
+++ html/includes/footer.inc.php
@@ -28,7 +28,7 @@
 			</div>
 		</div>
 	</div>
-	<div class='navbar navbar-fixed-bottom' style='text-align: center'>
+	<div class='col-sm-12' style='text-align: center; padding: 15px 0'>
 		<em>&copy 2015 University of Illinois Board of Trustees</em>
 	</div>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
