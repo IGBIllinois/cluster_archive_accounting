a7f0c5d1266b667094cc85782552bc072875d268
Joe Leigh <jleigh@illinois.edu>
2016-01-07 16:27:29 -0600

    Minor code cleanup

--- html/data_cost.php
+++
@@ -1,7 +0,0 @@
-<?php
-	require_once 'includes/header.inc.php';
-	
-?>
-
-<?php
-	require_once 'includes/footer.inc.php';
--- html/edit_transaction.php
+++ html/edit_transaction.php
@@ -1,5 +1,4 @@
 <?php
-	error_reporting(E_ALL);
 	require_once 'includes/header.inc.php';
 	if(!$login_user->is_admin()){
 		exit;
